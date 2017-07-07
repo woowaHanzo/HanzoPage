e5ec10cb9f5ba0d8cbab0dc50e4c367abbc203fa
Ubuntu <ubuntu@ip-172-31-9-46.ap-northeast-2.compute.internal>
2017-07-07 06:23:32 +0000

    Change design

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var express = require(“express”);
+var express = require("express");
 var app = express();
 
-app.use(express.static(“public”));
+app.use(express.static("public"));
 
-app.use(“/static”, express.static(__dirname + “/public”));
+app.use("/static", express.static(__dirname + "/public"));
 
 app.listen(3000, function () {
-    console.log(“Example app listening on port 3000!“);
+    console.log("Example app listening on port 3000!");
 });
